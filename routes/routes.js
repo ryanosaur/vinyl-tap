f382a368b0242d1360010402659f66ed3b7f8e51
Ryano <ryan.justin.taylor@gmail.com>
2015-07-12 22:07:09 -0700

    implemented routes for dealing with swappppppssss

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var Swap = require('../models/swap');
 
 var routes = function(app, passport) {
-  router.post("/users/:username/albums", function(req, res) {
+  router.post('/swaps', function(req, res, next){
+    var swap = new Swap(req.body);
+
+    swap.save(function(error, savedSwap){
+      if(error){
+        console.log(error);
+        res.status(400).json({ error: error });
+      }
+      res.json(savedSwap);
+    });
+  });
+
+  router.patch('/swaps/:id', function(req, res, next){
+    Swap.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body, { new: true },
+      function(error, updatedSwap){
+        if(error){
+          console.log(error);
+          res.status(400).json({ error: error });
+        }
+        if(!updatedSwap){
+          res.status(404).json({ error: 'Could not find swap' });
+        }
+        res.json(updatedSwap);
+      }
+    );
+  });
+
+  router.get('/swaps/from/:username', function(req, res, next){
+    Swap.find({ requester: req.params.username }).exec(function(error, swaps){
+      if(error){
+        console.log(error);
+        res.status(400).json({ error: error });
+      }
+      res.json(swaps);
+    });
+  });
+
+  router.get('/swaps/to/:username', function(req, res, next){
+    Swap.find({ owner: req.params.username }).exec(function(error, swaps){
+      if(error){
+        console.log(error);
+        res.status(400).json({ error: error });
+      }
+      res.json(swaps);
+    });
+  });
+
+  router.post('/users/:username/albums', function(req, res) {
     User.findOne({
       username: req.params.username
     }, function(error, user) {
