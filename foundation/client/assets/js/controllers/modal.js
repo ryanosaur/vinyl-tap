e5733a63b8eeef659c19a8bdb1b639db7d2fcfc3
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-07-13 03:06:52 -0700

    add edit album functionality

--- foundation/client/assets/js/controllers/modal.js
+++ foundation/client/assets/js/controllers/modal.js
@@ -2,9 +2,14 @@
   'use strict';
 
 angular.module('VINYLTAP.controller.modal', [])
-  .controller('ModalController', function($scope, $state, User) {
+  .controller('ModalController', function($scope, $state, User, Album) {
     $scope.saveEdit = function(){
-
+      Album.updateAlbum($scope.album.username, $scope.album._id, $scope.album).success(function(album){
+        $scope.editing = false;
+      })
+      .catch(function(error){
+        console.log(error);
+      });
     }
   });
 })();
