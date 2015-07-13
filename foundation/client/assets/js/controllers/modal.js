f1adfe2ed4f55b8945bc5cad5cc0b97c9120eb33
Ryano <ryan.justin.taylor@gmail.com>
2015-07-13 03:36:05 -0700

    added email type on login, got swaps posting, hide request swap when not logged in

--- foundation/client/assets/js/controllers/modal.js
+++ foundation/client/assets/js/controllers/modal.js
@@ -2,7 +2,17 @@
   'use strict';
 
 angular.module('VINYLTAP.controller.modal', [])
-  .controller('ModalController', function($scope, $state, User, Album) {
+  .controller('ModalController', function($scope, $state, User, Album, Swap) {
+    (function(){
+      $scope.activeUser = User.activeUser;
+    })();
+    $scope.createSwap = function(){
+      Swap.createSwap({
+        requester: $scope.activeUser.username,
+        owner: $scope.album.username,
+        owner_album: $scope.album._id
+      });
+    }
     $scope.saveEdit = function(){
       Album.updateAlbum($scope.album.username, $scope.album._id, $scope.album).success(function(album){
         $scope.editing = false;
--- models/swap.js
+++ models/swap.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 var swapSchema = mongoose.Schema({
     owner: { type: String, required: true },
     requester: { type: String, required: true },
-    owner_album: { type: Number, required: true },
+    owner_album: { type: String, required: true },
     requester_album: { type: String },
     createdAt: { type: Date, default: Date.now },
-    state: { type: String, default: 'active' }
+    state: { type: String, default: 'new' }
 });
 
 module.exports = mongoose.model('Swap', swapSchema);
