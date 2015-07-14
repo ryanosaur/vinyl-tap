4d14c798115012fc6e4ae1a1a909bf2b704819b2
ryanosaur <ryan.justin.taylor@gmail.com>
2015-07-14 15:17:18 -0700

    refactor the swapping function into chained promises instead of nested promises. execution is the same, just much more readable.

--- foundation/client/assets/js/controllers/swap.js
+++ foundation/client/assets/js/controllers/swap.js
@@ -12,31 +12,24 @@ angular.module('VINYLTAP.controller.swap', [])
       newOwnerAlbum.username = $scope.swap.owner;
       newOwnerAlbum.state = "done";
       Album.updateAlbum($scope.swap.requester, newOwnerAlbum._id, newOwnerAlbum)
-      .success(function(album){
+      .then(function(){
         //requester update
         var newRequesterAlbum = $scope.swap.owner_album;
         newRequesterAlbum.username = $scope.swap.requester;
         newRequesterAlbum.state = "done";
-        Album.updateAlbum($scope.swap.owner, newRequesterAlbum._id, newRequesterAlbum )
-        .success(function(album){
-          //update swap state
-          $scope.swap.state = "done";
-          Swap.updateSwap($scope.swap._id, $scope.swap)
-          .success(function(swap){
-            $scope.$apply();
-            $state.reload();
-          })
-          .catch(function(error){
-            console.log(error);
-          });
-        })
-        .catch(function(error){
-          console.log(error);
-        }); //end requester update
+        Album.updateAlbum($scope.swap.owner, newRequesterAlbum._id, newRequesterAlbum);
+      })
+      .then(function(){
+        //update swap state
+        $scope.swap.state = "done";
+        Swap.updateSwap($scope.swap._id, $scope.swap);
+      })
+      .then(function(){
+        $state.reload();
       })
       .catch(function(error){
         console.log(error);
-      }); //end owner update
+      });
     }
     $scope.$on('activeSwap', function(event, activeSwap){
       console.log($scope.swap);
